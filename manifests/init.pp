5afe8519ea932fe1a3b3a1b40033cdcd15988fc3
Andy Loftus <aloftus@gmail.com>
2023-08-26 01:24:50 +0000

    SVCPLAN-3738  JSM Puppetization
    Install and configure LVMs, Postgres, Apache
    SVCPLAN-3745  JSM Postgres Backups
    SVCPLAN-4004  Jira filesystem backups

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,15 +8,30 @@
 #
 # @param db_password Password for $db_user to access the database
 #
+# @param jira_home Jira [shared] home, absolute filesystem path
+#
+# @param backup_dir Absolute path where backups should go
+#
+# @param backups_max_qty Keep this many backups
+#
 # @example
 #   include profile_jsm
 class profile_jsm (
-  String $db_name,
-  String $db_user,
-  String $db_password,
+  String  $db_name,
+  String  $db_user,
+  String  $db_password,
+  String  $jira_home,
+  String  $backup_dir,
+  Integer $backups_max_qty,
 ) {
-  include ::lvm
-  include ::postgresql::server
+  include lvm
+  include profile_website
+
+  $cron_params = {
+    hour   => 4,
+    minute => 4,
+    user   => 'root',
+  }
 
   # This seems to be the only way to interact with postgresql::globals
   # but install fails regardless
@@ -25,19 +40,55 @@ class profile_jsm (
   #   manage_package_repo =>  true,
   #   version =>  '15',
   # }
-  # class { 'postgresql::server':
-  # }
+
+  ### Postgres setup
+  class { 'postgresql::server':
+    backup_enable  => true,
+    backup_options => {
+      dir         => "${backup_dir}/postgres",
+      db_user     => 'postgres_backup_user',
+      db_password => $db_password,
+      time        => [$cron_params[hour], $cron_params[minute]],
+      manage_user => true,
+      rotate      => $backups_max_qty,
+    },
+  }
 
   postgresql::server::database { $db_name :
     comment  => 'Jira Service Management',
-    encoding => 'UTF-8',
-    locale   => 'en_US.UTF-8',
+    encoding => 'UNICODE',
   }
 
+  $pwdhash = postgresql::postgresql_password( $db_user, $db_password )
   postgresql::server::role { $db_user :
-    password_hash => $db_password,
+    password_hash => $pwdhash,
     createdb      => true,
     db            => $db_name,
   }
 
+  postgresql::server::grant { $db_name :
+    privilege => 'ALL',
+    db        => $db_name,
+    role      => $db_user,
+  }
+
+  ### Jira Backups
+  $jira_backup = '/root/cron_scripts/jira-backup.sh'
+  file { $jira_backup :
+    ensure  => file,
+    mode    => '0700',
+    owner   => 'root',
+    group   => '0',
+    content => epp("profile_jsm/${jira_backup}.epp", {
+        jira_home  => $jira_home,
+        backup_dir => "${backup_dir}/jirahome",
+        rotate     => $backups_max_qty,
+      }
+    ),
+  }
+
+  cron { 'jira home backup':
+    command => $jira_backup,
+    *       => $cron_params,
+  }
 }
